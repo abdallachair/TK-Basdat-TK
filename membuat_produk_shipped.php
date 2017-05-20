5a56fcce9579d776bd97896df5e1a3b30cda93f8
abdallachair <abdallachair79@gmail.com>
2017-05-20 12:50:25 +0700

    shipped produk 70%

--- membuat_produk_shipped.php
+++ membuat_produk_shipped.php
@@ -1,18 +1,51 @@
 <?php
+    $_SESSION['toko'] = "Abram's Electronic";
+
     function selectAllFromTable($table) {
-
+        $DBConnection = pg_connect("host=localhost port=5432 dbname=abdallachair user=postgres password=abdall4");
         $query = "SELECT * FROM $table";
-
+        $result = pg_query($DBConnection, $query);
         return $result;   
     }
     
-    function insertProdukPulsa(){
-
+    function insertProdukShipped(){
+        $DBConnection = pg_connect("host=localhost port=5432 dbname=abdallachair user=postgres password=abdall4");
         $kode_produk = $_POST['kode_produk'];
         $nama_produk = $_POST['nama_produk'];
+        $toko = $_SESSION['toko'];
+        echo $toko;
+        $nama_toko =  str_replace("'", "''", $toko);
+        echo $nama_toko;
         $harga = $_POST['harga'];
         $deskripsi = $_POST['deskripsi'];
         $nominal = $_POST['nominal'];
+        $foto = basename($_FILES["file"]["name"]);
+        
+        echo $foto;
+        
+        if($_POST['asuransi'] === 'Ya'){
+            $asuransi = TRUE;
+        } else{
+            $asuransi = FALSE;
+        }
+                            
+        if($_POST['barang_baru'] === 'Ya'){
+            $barang_baru = TRUE;
+        } else{
+            $barang_baru = FALSE;
+            }
+                            
+        $min_order = $_POST['minimal_order'];
+        $min_grosir = $_POST['minimal_grosir'];
+        $max_grosir = $_POST['maksimal_grosir'];
+        $harga_grosir = $_POST['harga_grosir'];
+        $kategori = $_POST['kategori'];
+        
+        $query = "SELECT kode from TOKOKEREN.SUB_KATEGORI WHERE nama = '$kategori'";
+        
+        $result = pg_query($DBConnection, $query);
+        $kategori = pg_fetch_row($result);
+        $kategori = $kategori[0];
         
         $result1=is_numeric($harga);
         $result2=is_numeric($nominal);
@@ -25,6 +58,8 @@
             //echo '2';
             $_SESSION['errorMSG'] = "Harga harus angka dan lebih besar dari 0";
             //header("Location: menambah_produk_pulsa.php");
+        } else if(!is_numeric($min_order) && !is_numeric($min_grosir) && !is_numeric($max_grosir) && !is_numeric($harga_grosir) && ($harga_grosir||$min_order||$min_grosir||$maks_grosir) <= 0){
+            $_SESSION['errorMSG'] = "minimal order, minimal grosir, maksimal grosir, dan harga grosir harus berupa angka dan diatas nilai 0!";
         } else {
             // echo '3';
             $sql = "SELECT * FROM TOKOKEREN.SHIPPED_PRODUK SP, TOKOKEREN.PRODUK P WHERE SP.kode_produk = '$kode_produk' AND P.kode_produk = SP.kode_produk";
@@ -36,8 +71,12 @@
                 $sql2 = "INSERT INTO TOKOKEREN.PRODUK (kode_produk, nama, harga, deskripsi) VALUES ('$kode_produk', '$nama_produk', '$harga', '$deskripsi')";
                 $result2 = pg_query($DBConnection, $sql2);
             
-                $sql2 = "INSERT INTO TOKOKEREN.PRODUK_PULSA (kode_produk, nominal) VALUES ('$kode_produk', '$nominal')";
+                $sql2 = "INSERT INTO TOKOKEREN.SHIPPED_PRODUK (kode_produk, kategori, nama_toko, is_asuransi, stok, is_baru, min_order, min_grosir, max_grosir, harga_grosir, foto) VALUES ('$kode_produk', '$kategori', '$nama_toko', '$asuransi', '$nominal', '$barang_baru', '$min_order', '$min_grosir', '$max_grosir', '$harga_grosir', '$foto')";
                 $result2 = pg_query($DBConnection, $sql2);
+                
+                $file_tmp = $_FILES['file']['tmp_name'];
+                
+                move_uploaded_file($file_tmp, ("./src/img/".$foto));
             
                 $_SESSION['successMSG'] = "Berhasil membuat Produk!";
             }
@@ -50,10 +89,10 @@
         
         if($_POST['minimal_grosir'] > $_POST['maksimal_grosir']){
             $_SESSION['errorMSG'] = "Minimal Grosir tidak boleh lebih besar dari Maksimal Grosir!";
-        } else if(){
-               
-        } else if($_POST['command'] === 'membuat_produk_pulsa'){
-            insertProdukPulsa();
+        } else if($_POST['kategori'] === 'PILIH SUB KATEGORI'){
+            $_SESSION['errorMSG'] = "Kategori tidak boleh kosong!";
+        } else if($_POST['command'] === 'membuat_produk_shipped'){
+            insertProdukShipped();
         } 
     }
 ?>
@@ -77,10 +116,10 @@
         <div class="membuat_toko">
             <div class="container">
                 <h2>FORM MEMBUAT PRODUK PULSA</h2>
-                <form action="page.php" method="post">
+                <form action="membuat_produk_shipped.php" method="post">
                         <div class="form-group">
                             <label for="nama_paket">Kode_produk</label>
-                            <input type="text" class="form-control" id="insert-kode_produk" name="kode_produk" placeholder="masukkan kode produk" required>
+                            <input type="text" class="form-control" id="insert-kode_produk" maxlength="8" name="kode_produk" placeholder="masukkan kode produk" required>
                         </div>
                         <div class="form-group">
                             <label for="nama_paket">Nama produk</label>
@@ -96,12 +135,16 @@
                         </div>
                         <div class="form-group">
                             <label for="harga">Sub Kategori</label>
-                            <br> 
-                            <?php
-                                
-                            ?> 
-                            <select>
-                                <option>-----------------</option>
+                            <br>
+                            <select name="kategori">
+                                <?php
+                                    echo '<option>PILIH SUB KATEGORI</option>';
+                                    $sub = selectAllFromTable("TOKOKEREN.SUB_KATEGORI");
+                                    while($row = pg_fetch_row($sub)){
+                                        echo '<option>'.$row[2].'</option>';
+                                    }
+                                    echo '</select>';
+                                ?>
                             </select><br>
                         </div>
                         <div class="form-group">
@@ -136,23 +179,11 @@
                         </div>
                         <div class="form-group">
                             <label for="harga">Foto</label><span class="required" style="color: red">*</span></label>
-                            <input class="lagi " type="file" name="image_produk" id="image"/>
-                            <span name="error">
-                <?php  
-                    if(isset($_SESSION['errorSize'])){
-                        echo $_SESSION['errorSize'];
-                    } else if(isset($_SESSION['errorType'])){
-                        echo $_SESSION['errorType'];
-                    } else{
-                      //  print_r($_SESSION);
-                        unset($_SESSION['errorSize']);
-                        unset($_SESSION['errorType']);
-                    }
-                    
-                ?>          </span>
+                            <input class="lagi" type="file" name="file" id="file"/>
+                            <span name="error"></span>
                         </div>
                         <input type="hidden" id="insert-userid" name="userid">
-                        <input type="hidden" id="insert-command" name="command" value="membuat_produk_pulsa">
+                        <input type="hidden" id="insert-command" name="command" value="membuat_produk_shipped">
                         <button type="submit" class="btn btn-primary brown lighten-3">Submit</button>
                 </form>
             </div>
